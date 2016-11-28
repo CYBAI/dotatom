bf07d9639e3ea35961ad8c6bd01a187e8b2a9729
Bai.Chengyou | Haku | ASRHQ <chengyou.bai@rakuten.com>
2016-11-28 11:21:32 +0800

    Update installed packages

--- install.sh
+++ install.sh
@@ -1,4 +1,4 @@
-PACKAGES="file-icons linter-eslint nuclide autocomplete-paths language-babel minimap set-syntax docblockr language-docker minimap-git-diff seti-ui fast-eslint linter monokai-seti vim-mode-plus"
+PACKAGES="file-icons pigments set-syntax seti-ui vue-snippets linter linter-eslint linter-ruby nuclide autocomplete-paths language-vue language-rust language-pug language-haskell language-babel minimap set-syntax docblockr language-docker minimap-git-diff seti-ui fast-eslint linter monokai-seti vim-mode-plus"
 
 for PACKAGE in $PACKAGES
 do
